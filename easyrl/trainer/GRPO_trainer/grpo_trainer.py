ff9717de157e563df882f5ede8b3eca057dde77d
liunanfu <liunanfu1992@gmail.com>
2025-11-04 07:53:57 +0000

    <add> functional integration testing (inference and judge)

--- easyrl/trainer/GRPO_trainer/grpo_trainer.py
+++ easyrl/trainer/GRPO_trainer/grpo_trainer.py
@@ -1,16 +1,21 @@
 from easyrl.data_processor.training_data_processor import TrainingDataProcessor
 from easyrl.data_processor.validation_data_processor import ValidationDataProcessor
 from easyrl.inference_engine.inference_engine import InferenceEngine
-from time import sleep
+from easyrl.reward_verifier.math_verifier import MathVerifier
+from easyrl.advantage_calculator.group_advantage_calculator import GroupAdvantageCalculator
+import pandas as pd
 
 class GRPOTrainer:
-    def __init__(self, batch_size: int = 512, micro_batch_size: int = 64, epochs: int = 10):
+    def __init__(self, batch_size: int = 64, micro_batch_size: int = 8, epochs: int = 10, rollout_n: int = 8):
         self.batch_size = batch_size
         self.micro_batch_size = micro_batch_size
         self.epochs = epochs
+        self.rollout_n = rollout_n
         self.training_data_processor = TrainingDataProcessor(data_path='/run/determined/workdir/jiayanglyu/EasyRL/data/rl_train.parquet', batch_size=self.batch_size, micro_batch_size=self.micro_batch_size, epochs=self.epochs)
         self.validation_data_processor = ValidationDataProcessor(data_path='/run/determined/workdir/jiayanglyu/EasyRL/data/rl_valid.parquet')
-        self.inference_engine = InferenceEngine(model_path='/run/determined/workdir/jiayanglyu/model_zoo/Qwen2.5-7B')
+        self.inference_engine = InferenceEngine(model_path='/run/determined/workdir/jiayanglyu/model_zoo/Qwen2.5-7B', rollout_n=self.rollout_n)
+        self.math_verifier = MathVerifier()
+        self.group_advantage_calculator = GroupAdvantageCalculator()
     
     def fit(self):
         curr_index = 0
@@ -20,12 +25,17 @@ class GRPOTrainer:
             if curr_index % 10 == 0:
                 validation_group = self.validation_data_processor.get_validation_group()
                 validation_group = self.submit_validation_task_to_inference_engine(validation_group)
+                validation_group = self.verify_validation_answer(validation_group)
+                
 
             current_batch = self.training_data_processor.get_next_batch()
             current_batch = self.submit_training_task_to_inference_engine(current_batch)
+            current_batch = self.verify_training_answer(current_batch)
+            current_batch = self.compute_advantage(current_batch)
+
 
             self.inference_engine.sleep_engine()
-            
+
             curr_index += 1
  
 
@@ -60,6 +70,54 @@ class GRPOTrainer:
                 idx += 1
         return validation_group
     
+    def verify_validation_answer(self, validation_group):
+        judge_list = []
+        for group in validation_group:
+            for row in validation_group[group]['content']:
+                judge_list.append({'content': row['infer_content'][0], 'ground_truth': row['ground_truth']})
+        
+        judge_result = self.math_verifier.verify_answer(judge_list)
+        
+        idx = 0
+        for group in validation_group:
+            for row in validation_group[group]['content']:
+                row['reward'] = judge_result[idx]
+                idx += 1
+
+        return validation_group
+
+    def verify_training_answer(self, training_batch):
+        judge_list = []
+        for micro_batch in training_batch:
+            for row in micro_batch:
+                ground_truth = row['ground_truth']
+                for content in row['infer_content']:
+                    judge_list.append({'content': content, 'ground_truth': ground_truth})
+
+        rewards = self.math_verifier.verify_answer(judge_list)
+
+        idx = 0
+        for micro_batch in training_batch:
+            for row in micro_batch:
+                row['reward'] = rewards[idx:idx+self.rollout_n]
+                idx += self.rollout_n
+        return training_batch
+    
+    def compute_advantage(self, training_batch):
+        original_reward_batch = []
+        for micro_batch in training_batch:
+            for row in micro_batch:
+                original_reward_batch.append(row['reward'])
+        
+        advantages = self.group_advantage_calculator.compute_group_advantage(original_reward_batch)
+        
+        idx = 0
+        for micro_batch in training_batch:
+            for row in micro_batch:
+                row['advantage'] = advantages[idx]
+                idx += 1
+        
+        return training_batch
 
 
 if __name__ == "__main__":
