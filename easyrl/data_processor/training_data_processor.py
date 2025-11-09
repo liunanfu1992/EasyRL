2dfda3c103515115991d1f1fc4a144241d92c277
liunanfu1992 <liunanfu1992@gmail.com>
2025-11-09 08:58:42 +0000

    <modify> remove micro_batch concept from the frame

--- easyrl/data_processor/training_data_processor.py
+++ easyrl/data_processor/training_data_processor.py
@@ -6,11 +6,10 @@ from tqdm import tqdm
 logger = logging.getLogger(__name__)
 
 class TrainingDataProcessor:
-    def __init__(self, data_path: str, batch_size: int = 64, micro_batch_size: int = 8, new_columns: List[str] = [], epochs: int = 1):
+    def __init__(self, data_path: str, batch_size: int = 64, new_columns: List[str] = [], epochs: int = 1):
         self.columns: List[str] = ['prompt', 'ground_truth'] + new_columns
         self.data_pool: List[Dict[str, Any]] = self._load_dataset(data_path, epochs)
         self.batch_size: int = batch_size
-        self.micro_batch_size: int = micro_batch_size
         self._batch_gen: Optional[Iterator[List[Dict[str, Any]]]] = None
 
     def _load_dataset(self, data_path: str, epochs: int) -> List[Dict[str, Any]]:
@@ -34,13 +33,12 @@ class TrainingDataProcessor:
             batch = self.data_pool[i:i+self.batch_size]
             yield batch
 
-    def get_next_batch(self) -> List[List[Dict[str, Any]]]:
+    def get_next_batch(self) -> List[Dict[str, Any]]:
         if self._batch_gen is None:
             self._batch_gen = self._batch_generator()
 
         try:
             batch = next(self._batch_gen)
-            batch = [batch[i:i+self.micro_batch_size] for i in range(0, len(batch), self.micro_batch_size)]
             return batch
         except StopIteration:
             raise StopIteration("Training is completed")
